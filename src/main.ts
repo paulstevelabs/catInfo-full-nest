f79306a4f5c7f77ce0a5bc7cb0e8af4336eddd0a
paul <paul@stevelabs.co>
2022-06-01 20:15:12 +0900

    create swagger setting

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,30 @@ import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // class validator
+
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cat info')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+  SwaggerModule.setup('docs', app, document); // endpoint
+
+  app.enableCors({
+    origin: true, // 환경 변수 처리 필요
+    credentials: true,
+  });
+
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
